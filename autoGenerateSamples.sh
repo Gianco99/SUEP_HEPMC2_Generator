03fb2ca66581f0acfa5ca8f904c9a7ab7d89f992
Gianco99 <gianfrancocyclohexane@gmail.com>
2025-02-14 13:46:16 +0100

    DY generation, autogeneration, leptonic decay card

--- autoGenerateSamples.sh
+++ autoGenerateSamples.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set timeout -1
-spawn bash generateSamples.sh -f decay_darkphoton_generic.cmnd --mD 2.0 --T 2.0 -e /eos/user/g/gdecastr/madanalysis5/samples/SUEPs/Generic_2_2 -n 290 -c 1000
+spawn bash generateSamples.sh -f decay_darkphoton_generic.cmnd --mD 5.0 --T 5.0 -e /eos/cms/store/group/phys_exotica/SUEPs/ZH_HepMC/Generic_5_5/ -n 300 -c 1000
 expect {
     "Enter PEM pass phrase" {sleep 1; send "\r"; exp_continue }
     eof
