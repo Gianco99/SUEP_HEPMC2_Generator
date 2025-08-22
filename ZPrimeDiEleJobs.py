a2600d9e7d0fcb5a16e6f2f40389b94ce3ecbd3c
Gianfranco de Castro <gianco99@Gianfrancos-MacBook-Pro.local>
2025-08-21 20:06:56 -0400

    Updated autogen scripts

--- ZPrimeDiEleJobs.py
+++ ZPrimeDiEleJobs.py
@@ -52,8 +52,6 @@ def write_condor_submit(sub_path: str, scripts_dir: str, suppress_logs: bool = F
     lines = []
     lines.append("universe              = vanilla")
     lines.append(f'+JobFlavour          = "{JOB_FLAVOUR}"')
-    lines.append("getenv                = True")
-    lines.append("use_x509userproxy     = True")
     lines.append("transfer_executable   = True")
     # Ship the generate script to the worker
     lines.append("transfer_input_files  = generateZPrime.sh")
